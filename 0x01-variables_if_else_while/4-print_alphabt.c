0b3879fcd8eb1d0083379ade337bc30515f388aa
Cytotheworld <akcyril@yahoo.ca>
2023-02-17 05:51:21 -0800

    task4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - prints in lower case
+ *
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	char 'a';
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			if ((c != 'q' && c != 'e') && c <= 'z')
+				putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
