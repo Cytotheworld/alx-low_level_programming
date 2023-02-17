50c86f234346120927cdd3000c580e48557140c0
Cytotheworld <akcyril@yahoo.ca>
2023-02-17 07:38:43 -0800

    task 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,10 @@
 #include <stdio.h>
 /**
  * main - prints in lower case
+ *
  * Return: 0 (success)
  */
 
 int main(void)
 {
 	char 'a';
-
-	c = 'a';
-	while
-		(c <= 'z') {
-			if ((c != 'q' && c != 'e') && c <= 'z')
-				putchar(c);
-		}
-	putchar('\n');
-	return (0);
-}
