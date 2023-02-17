7e9c9f2027345e926a8fe905a4b005cb552955c0
Cytotheworld <akcyril@yahoo.ca>
2023-02-17 05:20:42 -0800

    task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints in lowercase then uppercase
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	char c;
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	while
+		(d <= 'z') {
+			putchar(c);
+			d++
+		}
+	return (0);
+}
