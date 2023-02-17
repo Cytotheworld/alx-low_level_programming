1f0215ed201ff273573271f00fb9096e28adcbb8
Cytotheworld <akcyril@yahoo.ca>
2023-02-17 06:20:03 -0800

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints a single digits number
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	for (a = 0; a < 10; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
